8b645df524c1c35021da1a51652cb51fb82f833c
michaelfarrell76 <mkfrl09@gmail.com>
2016-01-12 14:42:42 -0500

    Basic setup

--- bot/Player.py
+++ bot/Player.py
@@ -2,6 +2,16 @@ import argparse
 import socket
 import sys
 
+#Dictionary to split up parsing different packages
+options = {
+            "NEWGAME"           :   newGame,
+            "KEYVALUE"          :   keyValue,
+            "REQUESTKEYVALUES"  :   requestKeyValues,
+            "NEWHAND"           :   newHand,
+            "GETACTION"         :   getAction,
+            "HANDOVER"          :   handOver
+}
+
 """
 Simple example pokerbot, written in python.
 
@@ -10,38 +20,43 @@ necessary to connect with the engine and then always returns the same action.
 It is meant as an example of how a pokerbot should communicate with the engine.
 """
 class Player:
+    def __init__(self):
+        print "init"
+
     def run(self, input_socket):
+        
         # Get a file-object for reading packets from the socket.
         # Using this ensures that you get exactly one packet per read.
         f_in = input_socket.makefile()
+        
         while True:
             # Block until the engine sends us a packet.
             data = f_in.readline().strip()
+            
             # If data is None, connection has closed.
             if not data:
                 print "Gameover, engine disconnected."
                 break
 
-            # Here is where you should implement code to parse the packets from
-            # the engine and act on it. We are just printing it instead.
-            print data
-
-            # When appropriate, reply to the engine with a legal action.
-            # The engine will ignore all spurious responses.
-            # The engine will also check/fold for you if you return an
-            # illegal action.
-            # When sending responses, terminate each response with a newline
-            # character (\n) or your bot will hang!
+            #Determine packet type
             word = data.split()[0]
-            if word == "GETACTION":
-                # Currently CHECK on every move. You'll want to change this.
-                s.send("CHECK\n")
-            elif word == "REQUESTKEYVALUES":
-                # At the end, the engine will allow your bot save key/value pairs.
-                # Send FINISH to indicate you're done.
-                s.send("FINISH\n")
+
+            options[word](data)
+
         # Clean up the socket.
         s.close()
+        
+    def newGame(self, data):
+    def keyValue(self, data):
+    def requestKeyValues(self, data):
+        # At the end, the engine will allow your bot save key/value pairs.
+        # Send FINISH to indicate you're done.
+        s.send("FINISH\n")
+    def newHand(self, data):
+    def getAction(self, data):
+        # Currently CHECK on every move. You'll want to change this.
+        s.send("CHECK\n")
+    def handOver(self, data):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A Pokerbot.', add_help=False, prog='pokerbot')
